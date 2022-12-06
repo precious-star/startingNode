9a62484e307b2bb5f89f3a90151047716bad578d
Anmol.gupta <65755789+precious-star@users.noreply.github.com>
2022-12-06 18:02:48 +0530

    Update filtered-ls.js

--- filtered-ls.js
+++ filtered-ls.js
@@ -12,17 +12,3 @@ fs.readdir(process.argv[2], function callback(err, list) {
     }
 })
 
-// const fs = require('fs')
-// const path = require('path')
-
-// const folder = process.argv[2]
-// const ext = '.' + process.argv[3]
-
-// fs.readdir(folder, function (err, files) {
-//     if (err) return console.error(err)
-//     files.forEach(function (file) {
-//         if (path.extname(file) === ext) {
-//             console.log(file)
-//         }
-//     })
-// })
